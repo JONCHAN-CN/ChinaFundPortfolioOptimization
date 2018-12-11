15ec47f2401601ad63d5aae417a457bd883d3507
JONCHAN <skyvinz@live.cn>
2018-12-11 11:52:30 +0800

    V1

--- src/0-createFundTable.sql
+++ src/0-createFundTable.sql
@@ -1,4 +1,5 @@
-use invest ;
+--create DATABASE fund;
+use fund;
 DROP TABLE IF  EXISTS fund_info ;
 CREATE TABLE IF NOT EXISTS `fund_info` (
   `fund_code` varchar(255) NOT NULL COMMENT '基金代码',
@@ -11,7 +12,7 @@ CREATE TABLE IF NOT EXISTS `fund_info` (
   `asset_value` varchar(255) DEFAULT NULL COMMENT '最新资产规模',
   `asset_value_date` varchar(255) DEFAULT NULL COMMENT '最新资产规模日期',
   `units` varchar(255) DEFAULT NULL COMMENT '最新份额规模',
-  `units_date` varchar(255) DEFAULT NULL COMMENT '最新份额规模',
+  `units_date` varchar(255) DEFAULT NULL COMMENT '最新份额规模日期',
   `fund_manager` varchar(255) DEFAULT NULL COMMENT '基金管理人',
   `fund_trustee` varchar(255) DEFAULT NULL COMMENT '基金托管人',
   `funder` varchar(255) DEFAULT NULL COMMENT '基金经理人',
@@ -32,15 +33,40 @@ CREATE TABLE IF NOT EXISTS `fund_info` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='基金基本信息表';
  
  
+DROP TABLE IF  EXISTS fund_managers_chg; 
+CREATE TABLE IF NOT EXISTS `fund_managers_chg` (
+	`fund_code` varchar(255) NOT NULL COMMENT '基金代码',
+	`start_date` varchar(255) DEFAULT NULL COMMENT '起始期',
+	`end_date` varchar(255) DEFAULT NULL COMMENT '截止期', 
+	`fund_managers` varchar(255) DEFAULT NULL COMMENT '基金经理',
+	`term` varchar(255) DEFAULT NULL COMMENT '任职期间',
+	`return_rate` varchar(255) DEFAULT NULL COMMENT '任职回报',
+	`created_date` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+	`updated_date` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+	`data_source` varchar(255) DEFAULT NULL COMMENT '数据来源',
+	PRIMARY KEY (`fund_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='基金经理(based on fund)信息表';
+ 
+DROP TABLE IF  EXISTS fund_managers_info;
+ CREATE TABLE IF NOT EXISTS `fund_managers_info` (
+	`manager_id` varchar(255) NOT NULL COMMENT '基金经理ID',
+	`url` varchar(255) DEFAULT NULL COMMENT '链接',
+	`manager_name` varchar(255) DEFAULT NULL COMMENT '基金经理',
+	`created_date` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+	`updated_date` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+	`data_source` varchar(255) DEFAULT 'eastmoney' COMMENT '数据源',
+	PRIMARY KEY (`manager_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='基金经理(based on persons)信息表'; 
+ 
 DROP TABLE IF  EXISTS fund_nav ;
 CREATE TABLE IF NOT EXISTS `fund_nav` (
   `the_date` varchar(255) NOT NULL,
-  `nav` float(15,8) DEFAULT NULL,
-  `add_nav` float(15,8) DEFAULT NULL,
+  `nav` varchar(255) DEFAULT NULL,
+  `add_nav` varchar(255) DEFAULT NULL,
   `nav_chg_rate` varchar(255) DEFAULT NULL,
   `buy_state` varchar(255) DEFAULT NULL,
   `sell_state` varchar(255) DEFAULT NULL,
-  `div` varchar(255) DEFAULT NULL,
+  `div_record` varchar(255) DEFAULT NULL,
   `fund_code` varchar(255) NOT NULL,
   `created_date` datetime DEFAULT NULL,
   `updated_date` datetime DEFAULT NULL,
@@ -51,19 +77,13 @@ DROP TABLE IF  EXISTS fund_nav_currency ;
 CREATE TABLE IF NOT EXISTS  `fund_nav_currency` (
   `the_date` varchar(255) NOT NULL,
   `fund_code` varchar(255) NOT NULL,
-  `profit_per_units` float(15,8) DEFAULT NULL,
+  `profit_per_units` varchar(255) DEFAULT NULL,
   `profit_rate` varchar(255) DEFAULT NULL,
   `buy_state` varchar(255) DEFAULT NULL,
   `sell_state` varchar(255) DEFAULT NULL,
-  `div` varchar(255) DEFAULT NULL,
+  `div_record` varchar(255) DEFAULT NULL,
  
   `created_date` datetime DEFAULT NULL,
   `updated_date` datetime DEFAULT NULL,
   PRIMARY KEY (`the_date`,`fund_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---------------------- 
-作者：yuzhucu 
-来源：CSDN 
-原文：https://blog.csdn.net/yuzhucu/article/details/55261024 
-版权声明：本文为博主原创文章，转载请附上博文链接！
