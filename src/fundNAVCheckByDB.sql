1f46d5b9f38c8dcda7dfe50dcc81a3c89e3d5f7f
Jon <35148796+JONCHAN-CN@users.noreply.github.com>
2019-03-24 19:19:52 +0800

    Delete fundNAVCheckByDB.sql

--- src/fundNAVCheckByDB.sql
+++
@@ -1,7 +0,0 @@
-select fund_code, count(*)
-from (select distinct * from fund_nav) t
-group by fund_code
-INTO OUTFILE '1-fundCheckByDB.csv'
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n';
